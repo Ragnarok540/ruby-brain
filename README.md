4a7cda5b28cf62b5b8c4535b27791adb637101d0
Edgar Nova <ragnarok540@gmail.com>
2019-07-14 18:45:49 -0500

    CLI

--- README.md
+++ README.md
@@ -2,6 +2,27 @@
 
 Brainfuck interpreter in Ruby. Uses jump table and checks for unmatched []. 
 
+### Help
+
+```sh
+$ ruby main.rb --help
+usage: ruby main.rb [-h] [-v] file_name
+
+positional arguments:
+  file_name      brainfuck source code file
+
+optional arguments:
+  -h, --help     show this help message and exit
+  -v, --version  print version number on screen and exit
+```
+
+### Usage
+
+```sh
+$ ruby main.rb ./hello.bf
+Hello World!
+```
+
 ### Links
 
   * [Some brainfuck fluff.](http://www.hevanet.com/cristofd/brainfuck/)
--- src/main.rb
+++ src/main.rb
@@ -1,6 +1,31 @@
 require "./rbrain"
 
-# Hello World!
-bf = BrainFuck.new("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.")
+def process_argv(option)
+  case option
+    when "-h", "--help"
+      puts "usage: ruby main.rb [-h] [-v] file_name"
+      puts ""
+      puts "positional arguments:"
+      puts "  file_name      brainfuck source code file"
+      puts ""
+      puts "optional arguments:"
+      puts "  -h, --help     show this help message and exit"
+      puts "  -v, --version  print version number on screen and exit"
+      exit
+    when "-v", "--version"
+      puts "ruby-brain version 0.0.1 - Ruby BrainFuck Interpreter"
+      exit
+    else
+      @options[:file_name] = option
+  end
+end
+
+@options = {}
+
+ARGV.each { |option| process_argv(option) }
+
+code = File.read(@options[:file_name])
+
+bf = BrainFuck.new(code)
 
 bf.execute
